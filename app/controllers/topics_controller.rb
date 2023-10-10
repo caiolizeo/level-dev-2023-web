e932f45c98575a7707b464d023634489b8184b61
Caio Soares <caio.soares@locaweb.com.br>
2023-10-10 12:25:53 -0300

    add edit e destroy topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,51 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: %i[ show edit update destroy ]
+
   def index
     @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to topic_url(@topic), notice: 'Tópico criado'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+     if @topic.save
+      redirect_to topic_path(@topic), notice: 'Tópico criado'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @topic.destroy
+      redirect_to topics_path, notice: 'Tópico removido'
+    else
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:titulo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get '/topics', to: 'topics#index'
+  get "/topics", to: "topics#index"
+  post "/topics", to: "topics#create"
+  get "/topics/new", to: "topics#new", as: :new_topic
+  get "/topics/:id/edit", to: "topics#edit", as: :edit_topic
+  get "/topics/:id", to: "topics#show", as: :topic
+  patch "/topics/:id", to: "topics#update"
+  put "/topics/:id", to: "topics#update"
+  delete "/topics/:id", to: "topics#destroy"
   get '/', to: 'home#index'
 end
